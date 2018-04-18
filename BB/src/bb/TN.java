653ee88e6ebf29937baddfd6a908cec8e51a2613
Administrator <Administrator@192.168.1.105>
2018-04-19 02:48:06 +0700

    main

---
+++ BB/src/bb/TN.java
@@ -0,0 +1,124 @@
+package bb;
+import java.util.*;
+public class TN {
+public static void data(){
+// ข้อมูลที่จะเอาไปใส่
+System.out.println("Cosmetics");
+
+System.out.println("Lipstick");
+System.out.println("Under Tone");
+System.out.println("Eyeshadow");
+System.out.println("Primer");
+System.out.println("Concealer");
+//Maybelline
+System.out.println("Maybelline");
+    System.out.println("Lipstick");
+    //type
+    System.out.println("Lip Balm");
+        System.out.println("Babylips Loves Color Bright Out Loud");//89THB
+            System.out.println("Peach Bloom");
+            System.out.println("Pink Bloom");
+        System.out.println("Baby Lips Bloom");//99THB
+            System.out.println("Striking Orange");
+            System.out.println("Vivid Peach");
+            System.out.println("Wild Cherry");
+        System.out.println("Baby lips Candy Wow");//199THB
+            System.out.println("Blackberry");
+            System.out.println("Cherry");
+            System.out.println("Cinnamon");
+    System.out.println("Lipstick");
+        System.out.println("Lip Flush Bitten Lips");//229THB
+            System.out.println("PK02");
+            System.out.println("RD02");
+            System.out.println("RD01");
+        System.out.println("Super Stay Matte Ink {Kiss Proof}");//249THB
+            System.out.println("05 Loyalist");
+            System.out.println("10 Dreamer");
+            System.out.println("15 Lover");
+        System.out.println("Color Jolt");//269THB
+            System.out.println("02 Bittersweet Mandarin");
+            System.out.println("04 Show Off Nude");
+            System.out.println("05 You Red-Me");
+    
+    System.out.println("Foundation");
+    //type
+        System.out.println("Deram Satin Liquid Foundation SPF 24 PA+++");//449THB
+            System.out.println("02 Air Whipped");
+            System.out.println("01 ขาวเหลือง");
+            System.out.println("B00 ขาวชมพู");
+            System.out.println("B2 สีเนื้อ");
+        System.out.println("Dream Velvet Matte");//429THB
+            System.out.println("40 Nude");
+            System.out.println("05 Warm Procelaine");
+            System.out.println("10 เนื้ออ่อน");
+            System.out.println("60 เนื้อ");
+            System.out.println("75 เนื้อเข้ม");
+        System.out.println("FIT me! Matte+Poreless Liquid Foundation");
+            System.out.println("120");
+            System.out.println("122");
+            System.out.println("125");
+            System.out.println("128");
+            System.out.println("130");
+            System.out.println("220");
+            System.out.println("228");
+            System.out.println("230");
+            System.out.println("235");
+            System.out.println("238");
+            System.out.println("310");
+            System.out.println("322");
+    
+    System.out.println("Eyeshadow");
+    //type
+    System.out.println("The Nudes Palette");//549THB
+    System.out.println("The Blushed Nudes Palette");//549THB
+    System.out.println("The 24k Nudes Palette");//549THB
+    System.out.println("Gigi Hadid Eyeshadow Palette");//549THB
+    
+    System.out.println("Primer");
+    //type
+    System.out.println("Dream One Day Perfect");//379THB
+    System.out.println("Baby Skin ");//249THB
+    System.out.println("Baby Skin Pore Eraser");//329THB
+    
+    System.out.println("concealer");
+    //type
+    System.out.println("FIT me! Concealer");//249THB
+        System.out.println("10 Light");
+        System.out.println("15 Fair");
+        System.out.println("20 Sand");
+        System.out.println("25 Medium");
+        System.out.println("30 Cafe");
+        System.out.println("35 Deep");
+    System.out.println("Pure Minerals Healthy Natural Concealer");//219THB
+        System.out.println("1 Fresh");
+        System.out.println("3 Medium Sand");
+        System.out.println("2 Natural");
+    System.out.println("Master Camouflage duo Concealer");//229THB
+        System.out.println("Green Fawn");
+        System.out.println("Green Sand");
+        System.out.println("Peach Fawn");
+        System.out.println("PeaCH Sand");
+    System.out.println("Master Camoflage Concealer Palette");//549THB
+        System.out.println("Light");
+        System.out.println("Medium");
+    
+//Revlon        
+System.out.println("Revlon");
+    System.out.println("Lipstick");
+    //type
+    System.out.println("Lip Care");
+        System.out.println("Revlon Kiss Balm");///117THB
+            System.out.println("Tropical Coconut");
+            System.out.println("Juicy Peach");
+            System.out.println("Fresh Strawberry");
+            System.out.println("Sweet Cherry");
+            System.out.println("Crisp Apple");
+            System.out.println("Berry Burst");
+        System.out.println("New Revlon Kiss Exfoliating Balm ");//187THB
+            System.out.println("Sugar Mint");        
+    System.out.println("Lip Color");
+        System.out.println("");
+System.out.println("L'OREAL");
+
+}
+}
